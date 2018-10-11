b06059f56c000d80ee316a6e207b09b1716328c5
jjlouis <imjjlouis@gmail.com>
2018-10-11 21:12:25 +0800

    add:morse code

--- src/main/java/pri/jjlouis/leetcode/answer/UniqueMorseCodeWords.java
+++ src/main/java/pri/jjlouis/leetcode/answer/UniqueMorseCodeWords.java
@@ -32,15 +32,39 @@ package pri.jjlouis.leetcode.answer;
  * The length of words will be at most 100.
  * Each words[i] will have length in range [1, 12].
  * words[i] will only consist of lowercase letters.
+ * 研究下Hash算法
  */
 public class UniqueMorseCodeWords {
     private final static String[] MORSE_CODE = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    private final static byte[] MORSE_BYTE = new byte[MORSE_CODE.length];
-
+    private final static char[][] MORSE_CODE_CHAR_ARRAY = new char[MORSE_CODE.length][];
     static {
-        for(String code : MORSE_CODE){
-
+        for(int i = 0;i < MORSE_CODE.length;i++){
+            MORSE_CODE_CHAR_ARRAY[i] = MORSE_CODE[i].toCharArray();
         }
+    }
+
+    public static char[] getCode(String str){
+        char[] codes = new char[12 * 4];
+        if(str == null || str.length() == 0){
+            return null;
+        }
+        char[] chars = str.toCharArray(); // gin ->g
+        byte index = 1;
+        for(char ch : chars){
+            for(char co:MORSE_CODE_CHAR_ARRAY[ch - 97]){
+                chars[index++] = co;
+            }
+        }
+        chars[0] = (char)(index - 1);
+        return codes;
+    }
+
+    public int solution(String[] words){
+
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
