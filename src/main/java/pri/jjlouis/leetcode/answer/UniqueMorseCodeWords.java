3baca9ab80618cf8f7b4534b2d7244773f4b19c3
jjlouis <imjjlouis@gmail.com>
2019-03-13 16:49:58 +0800

    add:morse code

--- src/main/java/pri/jjlouis/leetcode/answer/UniqueMorseCodeWords.java
+++ src/main/java/pri/jjlouis/leetcode/answer/UniqueMorseCodeWords.java
@@ -33,13 +33,24 @@ package pri.jjlouis.leetcode.answer;
  * Each words[i] will have length in range [1, 12].
  * words[i] will only consist of lowercase letters.
  * 研究下Hash算法
+ *
+ * int a = 100  10进制表示
+ * int a = 0144 8进制表示
+ * int a = 0x100F 16进制表示
  */
 public class UniqueMorseCodeWords {
     private final static String[] MORSE_CODE = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    private final static char[][] MORSE_CODE_CHAR_ARRAY = new char[MORSE_CODE.length][];
+    private final static byte[] BYTES = new byte[MORSE_CODE.length];
+    private final static byte[]
     static {
         for(int i = 0;i < MORSE_CODE.length;i++){
-            MORSE_CODE_CHAR_ARRAY[i] = MORSE_CODE[i].toCharArray();
+            byte tmp =  0xFF;
+            byte[] chars= MORSE_CODE[i].getBytes();
+            for(int j = 0;j < chars.length;j++){
+                if(chars[j] == '.'){
+
+                }
+            }
         }
     }
 
